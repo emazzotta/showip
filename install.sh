5839df6a97fe602ffc378b37022b3f739882d922
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2017-06-02 22:21:53 +0200

    Add chmod to install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -fsSL https://raw.githubusercontent.com/emazzotta/showip/master/showip > /usr/local/bin/showip
+curl -fsSL https://raw.githubusercontent.com/emazzotta/showip/master/showip > /usr/local/bin/showip && chmod 755 /usr/local/bin/showip
 [[ $? == 0 ]] && echo "Successfully downloaded and installed" || echo "Problem downloading/installing"
